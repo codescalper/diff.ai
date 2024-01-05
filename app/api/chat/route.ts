b8d945cedbdc0197f3ec5663f58dc3aaeac2fe8f
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2024-01-05 06:51:01 +0000

    Formatted using prettier and fix lint issues

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,40 +1,41 @@
-import {Configuration,OpenAIApi } from 'openai-edge';
-import { OpenAIStream, StreamingTextResponse } from 'ai';
- 
+import { Configuration, OpenAIApi } from "openai-edge";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+
 const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
-const openai = new OpenAIApi(config); 
+const openai = new OpenAIApi(config);
 
-export const runtime = 'edge';
- 
+export const runtime = "edge";
+
 export async function POST(req: Request) {
-  try{
-  const { topic1, topic2, numberOfDiff, acronym
- } = await req.json();
- 
-  const response = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    stream: true,
-    messages: [
-      {
-        role: "user",
-        content: `Generate me a tabular format in markdown format This was about the content format.
+  try {
+    const { topic1, topic2, numberOfDiff, acronym } = await req.json();
+
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      stream: true,
+      messages: [
+        {
+          role: "user",
+          content: `Generate me a tabular format in markdown format This was about the content format.
         Now the content part: it should have ${numberOfDiff} differentiating points between the topics ${topic1} and ${topic2}. The table should have 3 columns: column1 - 'parameter' containing the subpoint of the differentiating point, column2 - ${topic1}, and column3 - ${topic2}.
-        ${acronym
- ? "Generate a ${numberOfDiff}-letter acronym based on the first letter of each parameter. The acronym should be after the tabular format and it should start with 'Here is the acronym for the parameters: (acronym word)' the '' the acronym word should be bold . Also explain what each letter of the acronym stands for from the parameter." : ""} `,
-      },
-    ],
-  });
- 
-     // Convert the response into a friendly text-stream
-     const stream = OpenAIStream(response);
-     // Respond with the stream
-     return new StreamingTextResponse(stream);
-   } catch (error) {
-     console.error(error);
-     // You can also return a response with an error message if you want
-     return new Response("An error occurred", { status: 500 });
-   }
- }
- 
+        ${
+          acronym
+            ? "Generate a ${numberOfDiff}-letter acronym based on the first letter of each parameter. The acronym should be after the tabular format and it should start with 'Here is the acronym for the parameters: (acronym word)' the '' the acronym word should be bold . Also explain what each letter of the acronym stands for from the parameter."
+            : ""
+        } `,
+        },
+      ],
+    });
+
+    // Convert the response into a friendly text-stream
+    const stream = OpenAIStream(response);
+    // Respond with the stream
+    return new StreamingTextResponse(stream);
+  } catch (error) {
+    console.error(error);
+    // You can also return a response with an error message if you want
+    return new Response("An error occurred", { status: 500 });
+  }
+}
--- components/Diff.tsx
+++ components/Diff.tsx
@@ -1,58 +1,51 @@
-'use client'
-import { Button } from "@/components/ui/button"
-import { Checkbox } from "@/components/ui/checkbox"
-import { Input } from "@/components/ui/input"
-import Image from "next/image"
+"use client";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
-import { toast as sonner } from "sonner"
-import { useToast } from "@/components/ui/use-toast"
-import { ToastAction } from "@/components/ui/toast"
+import { toast as sonner } from "sonner";
+import { useToast } from "@/components/ui/use-toast";
+import { ToastAction } from "@/components/ui/toast";
 import { useChat } from "ai/react";
 import ReactMarkdown from "react-markdown";
-import remarkGfm from 'remark-gfm'
+import remarkGfm from "remark-gfm";
 const Diff = () => {
+  const [acronym, setacronym] = useState<boolean>(false);
+  const [topic1, setTopic1] = useState<String>("");
+  const [topic2, setTopic2] = useState<String>("");
+  const [numberOfDiff, setNumberOfDiff] = useState<number>(0);
+  const { toast } = useToast();
+  const { messages, input, handleInputChange, handleSubmit, isLoading } =
+    useChat({
+      body: {
+        topic1,
+        topic2,
+        numberOfDiff,
+        acronym,
+      },
 
-   const [acronym
-, setacronym
-] = useState<boolean>(false);
-   const [topic1, setTopic1] = useState<String>('');
-   const  [topic2, setTopic2] = useState<String>('');
-   const [numberOfDiff, setNumberOfDiff] = useState<number>(0);
-   const { toast } = useToast()
-   const { messages, input,handleInputChange, handleSubmit, isLoading } =
-   useChat({
-     body: {
-      topic1,
-      topic2,
-      numberOfDiff,
-      acronym
+      onError: (err: any) => {
+        toast({
+          variant: "destructive",
+          title: "Uh oh! Something went wrong.",
+          description: `${err.message}`,
+          action: <ToastAction altText="Try again">Try again</ToastAction>,
+        });
+      },
+    });
 
-     },
-
-     
-  
-     onError: (err:any) => {
-      toast({
-        variant: "destructive",
-        title: "Uh oh! Something went wrong.",
-        description: `${err.message}`,
-        action: <ToastAction altText="Try again">Try again</ToastAction>,
-      })
-     },
-   });
-
-
-   const onSubmit = (e: any) => {
-    console.log("submitting")
+  const onSubmit = (e: any) => {
+    console.log("submitting");
     e.preventDefault();
     handleSubmit(e);
   };
-  console.log(topic1)
+  console.log(topic1);
   const lastMessage = messages[messages.length - 1];
-  console.log(lastMessage)
+  console.log(lastMessage);
 
   const generatedThread =
-  lastMessage?.role === "assistant" ? lastMessage.content : null;
+    lastMessage?.role === "assistant" ? lastMessage.content : null;
 
   const splittedArray = [];
   // Initialize the current index to 0
@@ -69,10 +62,10 @@ const Diff = () => {
     const firstPart = splitArray[0];
     // Update the currentIndex to the length of the first part
     currentIndex = firstPart.length;
-  
+
     // Push the first part to the array
     splittedArray.push(firstPart);
-  
+
     // Loop through the remaining parts
     for (let i = 1; i < splitArray.length; i++) {
       // Push the remaining parts to the array
@@ -80,7 +73,6 @@ const Diff = () => {
     }
   }
 
-  
   const bioRef = useRef<null | HTMLDivElement>(null);
 
   const scrollToBios = () => {
@@ -91,120 +83,146 @@ const Diff = () => {
 
   return (
     <div>
-          <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4">
-    <h1 className="sm:text-6xl text-4xl max-w-[708px] font-bold">
-      Discover <span className="text-gradient ">Differences{" "}</span> with diff.ai
-    </h1>
+      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4">
+        <h1 className="sm:text-6xl text-4xl max-w-[708px] font-bold">
+          Discover <span className="text-gradient ">Differences </span> with
+          diff.ai
+        </h1>
 
-    <p className="text-xl text-slate-300 mt-5">
-      An AI-powered tool to analyze and differentiate topics effortlessly.
-    </p>
-    <form>
-    <div className="max-w-xl w-full mt-10">
-      <div className="flex items-center space-x-3">
-        <Image
-          src="/1.png"
-          width={30}
-          height={30}
-          alt="Topic icon"
-          className="mb-5 sm:mb-0"
-        />
-        <p className="text-left  font-medium">
-          Enter your topics which you want to differentiate:
+        <p className="text-xl text-slate-300 mt-5">
+          An AI-powered tool to analyze and differentiate topics effortlessly.
         </p>
-      </div>
-      <div draggable='false' className="flex flex-col xl:flex-row items-center space-x-0 xl:space-x-3 mt-3">
-        <Input
-          type="text"
-          value={input}
-          onChange={handleInputChange}
-          placeholder="Rust"
-          className="w-full rounded-md shadow-sm focus:border-cyan-500 focus:shadow-sm focus:shadow-cyan-500 focus:ring-cyan-200 p-2"
-        />
-        <h1 className="font-bold text-glow">VS</h1>
-        <Input
-          type="text"
-          onChange={(e)=>setTopic2(e.target.value)}
-          placeholder="Go-lang"
-          className="w-full rounded-md shadow-sm mt-5 xl:mt-0 focus:border-cyan-500 focus:shadow-sm focus:shadow-cyan-500 focus:ring-cyan-200 p-2"
-        />
-    
-      </div>
+        <form>
+          <div className="max-w-xl w-full mt-10">
+            <div className="flex items-center space-x-3">
+              <Image
+                src="/1.png"
+                width={30}
+                height={30}
+                alt="Topic icon"
+                className="mb-5 sm:mb-0"
+              />
+              <p className="text-left  font-medium">
+                Enter your topics which you want to differentiate:
+              </p>
+            </div>
+            <div
+              draggable="false"
+              className="flex flex-col xl:flex-row items-center space-x-0 xl:space-x-3 mt-3"
+            >
+              <Input
+                type="text"
+                value={input}
+                onChange={handleInputChange}
+                placeholder="Rust"
+                className="w-full rounded-md shadow-sm focus:border-cyan-500 focus:shadow-sm focus:shadow-cyan-500 focus:ring-cyan-200 p-2"
+              />
+              <h1 className="font-bold text-glow">VS</h1>
+              <Input
+                type="text"
+                onChange={(e) => setTopic2(e.target.value)}
+                placeholder="Go-lang"
+                className="w-full rounded-md shadow-sm mt-5 xl:mt-0 focus:border-cyan-500 focus:shadow-sm focus:shadow-cyan-500 focus:ring-cyan-200 p-2"
+              />
+            </div>
 
-      <div className="flex items-center mt-5 xl:mt-10 space-x-3">
-        <Image
-          src="/2.png"
-          width={30}
-          height={30}
-          alt="Topic icon"
-          className="mb-5 sm:mb-0"
-        />
-        <p className="text-left font-medium">
-          Enter the number of differenitaion you want to see:
-        </p>
-      </div>
+            <div className="flex items-center mt-5 xl:mt-10 space-x-3">
+              <Image
+                src="/2.png"
+                width={30}
+                height={30}
+                alt="Topic icon"
+                className="mb-5 sm:mb-0"
+              />
+              <p className="text-left font-medium">
+                Enter the number of differenitaion you want to see:
+              </p>
+            </div>
 
-      <div className="flex items-center space-x-0 xl:space-x-3 mt-3">
-        <Input
-          type="number"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>)=>setNumberOfDiff(parseInt(e.target.value))}
-          placeholder="8"
-          className="w-full xl:w-1/2 rounded-md shadow-sm focus:border-cyan-500 focus:shadow-sm focus:shadow-cyan-500 focus:ring-cyan-200 p-2"
-        />
-       
-      </div>
+            <div className="flex items-center space-x-0 xl:space-x-3 mt-3">
+              <Input
+                type="number"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setNumberOfDiff(parseInt(e.target.value))
+                }
+                placeholder="8"
+                className="w-full xl:w-1/2 rounded-md shadow-sm focus:border-cyan-500 focus:shadow-sm focus:shadow-cyan-500 focus:ring-cyan-200 p-2"
+              />
+            </div>
 
-      <div className="flex items-center mt-5 xl:mt-10 space-x-3">
-        <Image
-          src="/3.png"
-          width={30}
-          height={30}
-          alt="Topic icon"
-          className="mb-5 sm:mb-0"
-        />
-        <p className="text-left font-medium">
-        Choose whether to include acronyms for differentiation:
-        </p>
-      </div>
+            <div className="flex items-center mt-5 xl:mt-10 space-x-3">
+              <Image
+                src="/3.png"
+                width={30}
+                height={30}
+                alt="Topic icon"
+                className="mb-5 sm:mb-0"
+              />
+              <p className="text-left font-medium">
+                Choose whether to include acronyms for differentiation:
+              </p>
+            </div>
 
-      <div className="flex items-center space-x-0 xl:space-x-3 mt-5">
-      <Checkbox id="acronym
-OrNot" onCheckedChange={() => setacronym
-(!acronym
-)} />
-      <label
-        htmlFor="acronym
+            <div className="flex items-center space-x-0 xl:space-x-3 mt-5">
+              <Checkbox
+                id="acronym
 OrNot"
-        className="text-sm pl-3 xl:pl-0 font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-      >
-        Acronyms?
-      </label>
-      </div>
-      {!isLoading && <Button onClick={onSubmit} className='font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-white/80 w-full' > Differentiate 🚀 </Button>}
-      {isLoading && (
-          <Button disabled className='font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-white/80 w-full'>
-              <svg aria-hidden="true" role="status" className="inline w-4 h-4 me-3 text-zinc-40" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="#1C64F2"/>
-              </svg> 
-              Differentiating...             
-          </Button>
+                onCheckedChange={() => setacronym(!acronym)}
+              />
+              <label
+                htmlFor="acronym
+OrNot"
+                className="text-sm pl-3 xl:pl-0 font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+                Acronyms?
+              </label>
+            </div>
+            {!isLoading && (
+              <Button
+                onClick={onSubmit}
+                className="font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-white/80 w-full"
+              >
+                {" "}
+                Differentiate 🚀{" "}
+              </Button>
             )}
-      </div>
-      </form>
-      <hr className="h-px bg-gray-700 border-1 dark:bg-gray-700" />
-</main>
+            {isLoading && (
+              <Button
+                disabled
+                className="font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-white/80 w-full"
+              >
+                <svg
+                  aria-hidden="true"
+                  role="status"
+                  className="inline w-4 h-4 me-3 text-zinc-40"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="#1C64F2"
+                  />
+                </svg>
+                Differentiating...
+              </Button>
+            )}
+          </div>
+        </form>
+        <hr className="h-px bg-gray-700 border-1 dark:bg-gray-700" />
+      </main>
 
-<output className="space-y-10 my-10 mt-5 xl:mt-8">
+      <output className="space-y-10 my-10 mt-5 xl:mt-8">
         {generatedThread && (
           <>
             <div>
-              <h2
-                className="xl:text-4xl text-3xl text-center font-bold mx-auto"
-          
-              >
-                 Here's Your <span className="text-gradient">Markdown</span>{" "}
-                 Table
+              <h2 className="xl:text-4xl text-3xl text-center font-bold mx-auto">
+                Here&rsquo;s Your <span className="text-gradient">Markdown</span>{" "}
+                Table
               </h2>
             </div>
 
@@ -221,8 +239,8 @@ OrNot"
                   // }}
                   key={index} // Use index as the key instead of generatedBio
                 >
-                   <style>
-                  {`
+                  <style>
+                    {`
                     table {
                       border-collapse: collapse;
                       width: 100%;
@@ -236,8 +254,13 @@ OrNot"
                       text-align: left;
                     }
                   `}
-                </style>
-                  <p> <ReactMarkdown remarkPlugins={[remarkGfm]}>{generatedBio}</ReactMarkdown></p>
+                  </style>
+                  <p>
+                    {" "}
+                    <ReactMarkdown remarkPlugins={[remarkGfm]}>
+                      {generatedBio}
+                    </ReactMarkdown>
+                  </p>
                 </div>
               ))}
             </div>
@@ -245,7 +268,7 @@ OrNot"
         )}
       </output>
     </div>
-  )
-}
+  );
+};
 
-export default Diff
+export default Diff;
